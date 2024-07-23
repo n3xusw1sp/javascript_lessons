c60f11fb87b5028cbf34b2bb168be57b8b74a3d6
n3xusw1sp <n3xusw1sp@gmail.com>
2024-07-23 16:51:22 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// Lesson: https://youtu.be/maPRR_jjyOE?si=YlpHToUL7WBLEzRe
+
+// Урок #1 (Коментарі. Вивід інформації):
+// Час зупники:
+
+// Вивід інформації:
+console.log("Hello, from JS");
+
+// Коментарі:
+// #1: "//(код)"
+// #2: "/*(код)*/"
+
+
+// Урок #2 (Змінні):
+// Час зупинки:
+
+// Типи змінних:
+// let - можуть міняти значення поки програма виконується
+// var - аналог let, тільки старіший
+// const - змінна яка не може міняти значення
+
+// Приклад роботи змінної "let":
+let userName = 'Ihor'; // Початкове значення змінної
+userName = 'Nazar'; // Значення змінної змінилось
+console.log(userName);
+
+// Приклад роботи змінної "Const":
+const yearOfBirt = 2009;
+console.log(yearOfBirt);
+
+// Приклад роботи змінної "Var":
+var city = 'London';
+city = 'Kyiv';
+city = 'Lviv'
+console.log(city)
+
+// Також можна ще так створювати змінні:
+let name, age;
+name = "Ihor";
