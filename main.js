40bc17167f030abe0e378215e0fc4785987ee10f
n3xusw1sp <n3xusw1sp@gmail.com>
2024-07-24 20:25:51 +0300

    second commit

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // Час зупники:
 
 // Вивід інформації:
-console.log("Hello, from JS");
+// console.log("Hello, from JS");
 
 // Коментарі:
 // #1: "//(код)"
@@ -16,24 +16,35 @@ console.log("Hello, from JS");
 
 // Типи змінних:
 // let - можуть міняти значення поки програма виконується
-// var - аналог let, тільки старіший
+// var - аналог let, тільки старіший. Також має область видимості за межами функції
 // const - змінна яка не може міняти значення
 
 // Приклад роботи змінної "let":
-let userName = 'Ihor'; // Початкове значення змінної
-userName = 'Nazar'; // Значення змінної змінилось
-console.log(userName);
+// let userName = 'Ihor'; // Початкове значення змінної
+// userName = 'Nazar'; // Значення змінної змінилось
+// console.log(userName);
 
 // Приклад роботи змінної "Const":
-const yearOfBirt = 2009;
-console.log(yearOfBirt);
+// const yearOfBirt = 2009;
+// console.log(yearOfBirt);
 
 // Приклад роботи змінної "Var":
-var city = 'London';
-city = 'Kyiv';
-city = 'Lviv'
-console.log(city)
+// var city = 'London';
+// city = 'Kyiv';
+// city = 'Lviv'
+// console.log(city)
 
 // Також можна ще так створювати змінні:
-let name, age;
-name = "Ihor";
+// let name, age;
+// name = "Ihor";
+
+
+// Урок 3 (Типи данних):
+// Час зупинки:
+
+// Різниця між данними в лапках і без них:
+let age1 = 30;
+console.log(age1);
+
+let age2 = '30';
+console.log(age2);
